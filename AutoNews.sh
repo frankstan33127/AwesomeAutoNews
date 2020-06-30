a7820bdf3555a3046bda87d0d389f7d6fb2f407b
frankstan33127 <62330183+frankstan33127@users.noreply.github.com>
2020-06-30 19:53:33 +0530

    Update AutoNews.sh

--- AutoNews.sh
+++ AutoNews.sh
@@ -3,10 +3,9 @@ cd /home/$USER/AwesomeAutoNews/
 STR=0
 #Set the FREQ variable to set the amount of times you need to get the updates
 FREQ=10
-cd 
 while (( $STR < $FREQ ))
 do
 $((++STR))
-./AutoNews.py
+python3 AutoNews.py
 sleep 1h
 done
